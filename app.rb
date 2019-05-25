a47d8264bc8988901898aea2616eaf0e00ebdfe7
Niklas Stephenson <niklas@stephenson.dk>
2019-05-25 18:36:53 +0200

    Connect to Redis on Heroku

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@ require 'sidekiq/api'
 require 'redis'
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}" }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
-$redis = Redis.new( url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}" )
+$redis = Redis.new( url: ENV['REDIS_URL'] )
 
 class App < Sinatra::Application
   get '/' do
